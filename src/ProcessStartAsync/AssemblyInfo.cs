1bbec91878164fc004bdc97d5f30c95f72e3101a
Martin Jarvis <29016371+martinjarvis@users.noreply.github.com>
2018-08-01 20:34:22 +0100

    CodeCov integration test and AssemblyInfo method for GitVersioning

---
+++ src/ProcessStartAsync/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("ProcessStartAsync")]
+[assembly: AssemblyTitle("Converts executing a command line process into an Cancellable Task")]
+[assembly: AssemblyDescription("A handy ProcessStartInfo extension method for asynchronously calling a command line executable and monitoring their output")]
+[assembly: AssemblyCompany("MCJ Development Ltd")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0-alpha")]
+[assembly: AssemblyVersion("0.0.0.0")]
+
+#if RELEASE
+
+[assembly: AssemblyConfiguration("Release")]
+
+#else
+
+[assembly: AssemblyConfiguration("Debug")]
+
+#endif
