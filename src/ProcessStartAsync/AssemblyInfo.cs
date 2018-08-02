4073707c208b54b3bba6d5a52eb8bbbd96dae6c0
Martin Jarvis <29016371+martinjarvis@users.noreply.github.com>
2018-08-02 20:50:55 +0100

    Change library target framework to netstandard1.3

--- src/ProcessStartAsync/AssemblyInfo.cs
+++ src/ProcessStartAsync/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyTitle("Converts executing a command line process into an Cancellable Task")]
 [assembly: AssemblyDescription("A handy ProcessStartInfo extension method for asynchronously calling a command line executable and monitoring their output")]
 [assembly: AssemblyCompany("MCJ Development Ltd")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 MCJ Development Ltd")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0.0-alpha")]
 [assembly: AssemblyVersion("0.0.0.0")]
