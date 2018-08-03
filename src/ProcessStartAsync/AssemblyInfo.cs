bce2d6e7c0cb2e995cecd2bbe3421f19ce44ee82
Martin Jarvis <29016371+martinjarvis@users.noreply.github.com>
2018-08-03 18:31:26 +0100

    Attempt to get properly versioned nuget package on AppVeyor

--- src/ProcessStartAsync/AssemblyInfo.cs
+++ src/ProcessStartAsync/AssemblyInfo.cs
@@ -5,9 +5,9 @@
 [assembly: AssemblyDescription("A handy ProcessStartInfo extension method for asynchronously calling a command line executable and monitoring their output")]
 [assembly: AssemblyCompany("MCJ Development Ltd")]
 [assembly: AssemblyCopyright("Copyright © 2018 MCJ Development Ltd")]
+[assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0.0-alpha")]
-[assembly: AssemblyVersion("0.0.0.0")]
 
 #if RELEASE
 
